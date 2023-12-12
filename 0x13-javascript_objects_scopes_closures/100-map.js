c7759d1a031afd8cd41fe0cb2dfe1b645af90004
Chris Halogen <christackoms@gmail.com>
2023-12-12 23:58:06 +0100

    new map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 const list = require("./100-data").list;
-const new_list = list.map(function (num, index) {
+const newList = list.map(function (num, index) {
   return num * index;
 });
 
 console.log(list);
-console.log(new_list);
+console.log(newList);
