d65ad7fc63e5b37d6501087e776c4f68c021d536
Chris Halogen <christackoms@gmail.com>
2023-12-17 23:10:50 +0100

    Fix ORM 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,12 @@ import sys
 
 
 if __name__ == "__main__":
-    db_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = db_connect.cursor()
-    cursor.execute("SELECT * FROM states")
-    rows = cursor.fetchall()
-    for single_row in rows:
-        print(single_row)
-    cursor.close()
-    db_connect.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
