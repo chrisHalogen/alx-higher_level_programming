6125c832d14e823d5fd4069b6e34b0c82c49291d
Chris Halogen <christackoms@gmail.com>
2023-12-17 23:22:07 +0100

    No 5 Python ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A script that lists all states from the database hbtn_0e_0_usa """
+""" Python ORM Exercise """
 import MySQLdb
 import sys
 
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     db_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db_connect.cursor()
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("""SELECT cities.name FROM cities INNER JOIN states
+                   ON states.id=cities.state_id WHERE 
+                   states.name=%s""", (sys.argv[4],))
     rows = cursor.fetchall()
-    for single_row in rows:
-        print(single_row)
+    output_str_list = list(single_row[0] for single_row in rows)
+    print(*output_str_list, sep=", ")
     cursor.close()
     db_connect.close()
